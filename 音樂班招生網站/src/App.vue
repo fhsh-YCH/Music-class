14458fdd942b3b1413546c110f79e515d2450fd9
Kiki Yang <121238071+fhsh-YCH@users.noreply.github.com>
2023-10-02 00:11:48 +0800

    10/1 upload
    still need to edit

---
+++ 音樂班招生網站/src/App.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="overflow-x-hidden">
+    <div class="relative w-screen overflow-x-hidden bg-[#DBCEBB]">
+      <div class="min-h-screen flex flex-col items-center">
+        <!-- 棋盤格 -->
+        <div class="w-full h-[5vw] bg-[#7D5936] flex">
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+        </div>
+        <!-- title -->
+        <div
+          class="w-[56vw] h-[37.8vw] bg-opacity-80 bg-[#FFFFFF] mt-[5vw] text-center rounded-[2vw] flex flex-col items-center justify-center overflow-hidden"
+        >
+          <div
+            class="absolute w-[2vw] h-[2vw] rounded-full bg-[#AC947D] mt-[-30vw] ml-[-48vw]"
+          ></div>
+          <div
+            class="absolute w-[2vw] h-[2vw] rounded-full bg-[#AC947D] mt-[-30vw] ml-[48vw]"
+          ></div>
+          <div
+            class="absolute w-[2vw] h-[2vw] rounded-full bg-[#AC947D] mt-[30vw] ml-[48vw]"
+          ></div>
+          <div
+            class="absolute w-[2vw] h-[2vw] rounded-full bg-[#AC947D] mt-[30vw] ml-[-48vw]"
+          ></div>
+          <p class="font-bold text-[2.5vw]">臺北市立復興高級中學112學年度</p>
+          <p class="font-black text-[3.5vw] my-[2.5vw]">
+            音樂班甄選入學 暨術科測驗
+          </p>
+          <p class="font-bold text-[2.5vw]">招生資訊網</p>
+        </div>
+
+        <!-- Information -->
+        <p class="text-[6vw] font-bold pt-[8vw] pb-[0.01vw] text-[#AA483B]">
+          最新公告
+        </p>
+        <p class="text-[2.8vw] font-light pb-[3vw] text-[#AA483B]">
+          Latest News
+        </p>
+        <!-- component 1 -->
+        <div class="flex flex-col items-center mt-[5vw]">
+          <div class="flex items-center justify-center">
+            <div
+              class="w-[10vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] flex items-center justify-center overflow-hidden text-[3.5vw] text-[#7D5936] font-bold"
+            >
+              1/4
+            </div>
+            <div
+              class="w-[45vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] ml-[0.8vw] px-[2vw] flex items-center justify-center overflow-hidden text-[2vw] font-bold"
+            >
+              臺北市立復興高級中學112學年度戲劇班甄選入學簡章公告
+            </div>
+          </div>
+          <button
+            class="w-[56vw] h-[4vw] rounded-[10vw] mt-[1.5vw] bg-[#7D5936] text-[1.8vw] text-white overflow-hidden drop-shadow-lg"
+          >
+            網路公告
+          </button>
+        </div>
+        <!-- component 2 -->
+        <div class="flex flex-col items-center mt-[10vw]">
+          <div class="flex items-center justify-center">
+            <div
+              class="w-[10vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] flex items-center justify-center overflow-hidden text-[3.5vw] text-[#7D5936] font-bold"
+            >
+              1/4
+            </div>
+            <div
+              class="w-[45vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] ml-[0.8vw] px-[2vw] flex items-center justify-center overflow-hidden text-[2vw] font-bold"
+            >
+              臺北市立復興高級中學112學年度戲劇班甄選入學簡章公告
+            </div>
+          </div>
+          <button
+            class="w-[56vw] h-[4vw] rounded-[10vw] mt-[1.5vw] bg-[#7D5936] text-[1.8vw] text-white overflow-hidden drop-shadow-lg"
+          >
+            網路公告
+          </button>
+        </div>
+
+        <!-- component 3 -->
+        <div class="flex flex-col items-center mt-[10vw]">
+          <div class="flex items-center justify-center">
+            <div
+              class="w-[10vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] flex items-center justify-center overflow-hidden text-[3.5vw] text-[#7D5936] font-bold"
+            >
+              1/4
+            </div>
+            <div
+              class="w-[45vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] ml-[0.8vw] px-[2vw] flex items-center justify-center overflow-hidden text-[2vw] font-bold"
+            >
+              臺北市立復興高級中學112學年度戲劇班甄選入學簡章公告
+            </div>
+          </div>
+          <button
+            class="w-[56vw] h-[4vw] rounded-[10vw] mt-[1.5vw] bg-[#7D5936] text-[1.8vw] text-white overflow-hidden drop-shadow-lg"
+          >
+            網路公告
+          </button>
+        </div>
+        <!-- component 4 -->
+        <div class="flex flex-col items-center mt-[10vw]">
+          <div class="flex items-center justify-center">
+            <div
+              class="w-[10vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] flex items-center justify-center overflow-hidden text-[3.5vw] text-[#7D5936] font-bold"
+            >
+              1/4
+            </div>
+            <div
+              class="w-[45vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] ml-[0.8vw] px-[2vw] flex items-center justify-center overflow-hidden text-[2vw] font-bold"
+            >
+              臺北市立復興高級中學112學年度戲劇班甄選入學簡章公告
+            </div>
+          </div>
+          <button
+            class="w-[56vw] h-[4vw] rounded-[10vw] mt-[1.5vw] bg-[#7D5936] text-[1.8vw] text-white overflow-hidden drop-shadow-lg"
+          >
+            網路公告
+          </button>
+        </div>
+        <!-- Footer -->
+        <div
+          class="mt-[8vw] border-2 border-solid border-[#7D5936] rounded-md w-[70vw] h-[35vw] pt-[2.8vw] flex flex-col items-center"
+        >
+          <div class="justify-center text-[2.8vw] text-[#7D5936] font-bold">
+            <p>主辦學校：臺北市立復興高級中學</p>
+            <p>主辦單位：藝術組</p>
+            <p class="text-[#AA483B]">
+              連絡電話：(02)2891-4131分機8101 \ 8102藝術組
+            </p>
+          </div>
+          <!-- web link -->
+          <div class="flex justify-center items-center mt-[2vw]">
+            <button
+              class="w-[18vw] h-[6vw] border-[#6F5B49] border-[0.6vw] rounded-md mr-[17vw] justify-center text-[#AA483B] font-bold text-[3vw]"
+            >
+              音樂班官網
+            </button>
+            <button
+              href=""
+              class="w-[18vw] h-[6vw] border-[#6F5B49] border-[0.6vw] rounded-md justify-center text-[#AA483B] font-bold text-[3vw]"
+            >
+              FB專頁
+            </button>
+          </div>
+
+          <div class="mt-[2vw] flex">
+            <p class="text-[2vw] text-[#7D5936] font-light pr-[30vw]">
+              Designed :
+            </p>
+            <p class="text-[2vw] text-[#7D5936] font-light">Mentored : 陳 晉</p>
+          </div>
+          <div class="mt-[1vw] flex">
+            <p class="text-[2vw] text-[#7D5936] font-light">Created :</p>
+            <p class="text-[2vw] text-[#7D5936] font-light pl-[23vw]">
+              Powered : FHSH IT DEPT
+            </p>
+          </div>
+        </div>
+
+        <!-- 棋盤格 -->
+        <div class="w-full h-[5vw] bg-[#7D5936] mt-[5vw] flex">
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white"></div>
+          <div class="w-[2.5vw] h-[2.5vw] bg-white mt-[2.5vw]"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ 音樂班招生網站/src/components/isOpen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center mt-[5vw]">
+    <div class="flex items-center justify-center">
+      <div
+        class="w-[10vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] flex items-center justify-center overflow-hidden text-[3.5vw] text-[#7D5936] font-bold"
+      >
+        1/4
+      </div>
+      <div
+        class="w-[45vw] h-[12vw] rounded-[1.2vw] bg-[#FFFFFF] ml-[0.8vw] px-[2vw] flex items-center justify-center overflow-hidden text-[2vw] font-bold"
+      >
+        臺北市立復興高級中學112學年度戲劇班甄選入學簡章公告
+      </div>
+    </div>
+    <button
+      class="w-[56vw] h-[4vw] rounded-[10vw] mt-[1.5vw] bg-[#7D5936] text-[1.8vw] text-white overflow-hidden drop-shadow-lg"
+    >
+      網路公告
+    </button>
+  </div>
+</template>
